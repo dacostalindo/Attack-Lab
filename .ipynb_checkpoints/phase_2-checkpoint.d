0ed8b968b2a8df2c84f1003c3f7ccd78cf4882d2
Manuel Lindo <mali3819@colorado.edu>
2020-03-02 18:44:27 +0000

    all files

---
+++ .ipynb_checkpoints/phase_2-checkpoint.d
@@ -0,0 +1,10 @@
+
+phase_2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 8b 3c 25 ae a6 5e 	mov    0x3c5ea6ae,%rdi
+   7:	3c 
+   8:	c3                   	retq   
---
+++ phase_3.s
@@ -0,0 +1,2 @@
+movq $0x5565d0c0,%rdi
+retq
